29bc000f982f279613ed1038e8d9f231f1de7004
MatheusStrilicherkDEV <matheuscherk@hotmail.com>
2020-11-24 16:24:05 -0300

    Exercício 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nascimento;
+            int atual;
+            int idade;
+            Console.Write("Digite o ano em que você nasceu: ");
+            nascimento = int.Parse(Console.ReadLine());
+            Console.Write("Digite o ano atual: ");
+            atual = int.Parse(Console.ReadLine());
+
+            idade=atual-nascimento;
+            if (idade>=16)
+            {
+                Console.WriteLine("Você está apto a votar");
+            } else
+            {
+              Console.WriteLine("Você ainda não possui idade suficiente para votar");
+            }
+        }
+    }
+}
+
